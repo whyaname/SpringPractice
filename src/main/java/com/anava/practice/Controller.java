939e9904078a8ea499ea2e82f138d3d1e64f2ce5
Anava Jain <anavajain@Anavas-MBP.fritz.box>
2020-08-16 16:17:34 +0200

    Initial commit

---
+++ src/main/java/com/anava/practice/Controller.java
@@ -0,0 +1,14 @@
+package com.anava.practice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Spring Boot";
+    }
+
+}
