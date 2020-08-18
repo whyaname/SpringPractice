4f558f82bffb5eeb32cc6d908f73be2e50a30d43
git <info@edwincox.nl>
2020-08-18 20:32:23 +0200

    - Nieuwe opdracht

--- src/main/java/com/anava/practice/Controller.java
+++ src/main/java/com/anava/practice/Controller.java
@@ -11,4 +11,18 @@ public class Controller {
         return "Hello Spring Boot";
     }
 
+    // Maak nu een GET methode, dit betekend dat ik niet via POSTMAN de waarde kan opvragen met een PUT POST
+    // Jou gemaakte methode kun je POST GET PUT ETC benaderen probeer dit veiliger te maken
+
+
+    // Maak een POST Mapping
+    // Wanneer ik dan een getal mee geef in de url moet ik deze x2 terug krijgen.
+    // Bijvoorbeeld ik geef getal 2
+    // wil ik antwoord 4 terug
+
+    // Zorg dat de Controller class in de packge controller komt
+
+    // Hernoem de PractiveApplication naar Application
+
+    // Goed gemaakt!
 }
