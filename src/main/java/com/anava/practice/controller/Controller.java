a68587f76fd1d4a81825ee4b652b4a70c1ab20d5
whyaname <anavajain@gmail.com>
2020-08-25 10:16:09 +0200

    Added PostMapping and GetMapping, renamed packages and files.

--- src/main/java/com/anava/practice/controller/Controller.java
+++ src/main/java/com/anava/practice/controller/Controller.java
@@ -1,7 +1,6 @@
-package com.anava.practice;
+package com.anava.practice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
@@ -14,15 +13,22 @@ public class Controller {
     // Maak nu een GET methode, dit betekend dat ik niet via POSTMAN de waarde kan opvragen met een PUT POST
     // Jou gemaakte methode kun je POST GET PUT ETC benaderen probeer dit veiliger te maken
 
+    // Ik krijg een METHOD NOT SUPPORTED of BAD REQUEST als ik een PUT of POST probeer, maar om het veiliger te maken
+    // dacht ik om de redirect te verwijderen, is dat een goede keuze?
+
+    @GetMapping("/get")
+    public String mapping() {
+        return "Get Mapping test";
+    }
 
     // Maak een POST Mapping
     // Wanneer ik dan een getal mee geef in de url moet ik deze x2 terug krijgen.
     // Bijvoorbeeld ik geef getal 2
     // wil ik antwoord 4 terug
 
-    // Zorg dat de Controller class in de packge controller komt
+    @PostMapping("/{number}")
+    public int multiply(@PathVariable int number) {
+        return (number*2);
+    }
 
-    // Hernoem de PractiveApplication naar Application
-
-    // Goed gemaakt!
 }
