3b43cc676b78c23ad6b0ed60aa7f8fad16d8f932
git <info@edwincox.nl>
2020-08-27 21:26:53 +0200

    - Opdracht 2

--- src/main/java/com/anava/practice/controller/Controller.java
+++ src/main/java/com/anava/practice/controller/Controller.java
@@ -31,4 +31,14 @@ public class Controller {
         return (number*2);
     }
 
+    // 1) Maak in de map een nieuwe controller noem deze BankController
+    // 2) Maak een endpoint met POST om een account voor een klant aan te maken
+    // 3) maak een service class aan wanneer Zdoat je vanuit de Post binnen komt in de controller ga je door naar de service, in de service wordt
+    // aan de hand van de gegevens een user Object aangemaakt, daarna retour
+    // Goed bezig! Zijn de kleine basis stappen, waar het naar toe gaat weet ik ook niet :-)
+
+    // Voor Later:
+    // 4) Model Customer , welke properties heeft een klant nodig?  voornaam achternaam postcode adres wooenplaats huisnummer  bankrekeningNummer
+
+
 }
